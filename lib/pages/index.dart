bbb6f9aeda3049ad69578d50914a636d00a443b4
viniciushrk <viniciushrk@gmail.com>
2023-10-31 18:14:34 -0400

    feat: create consume api

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,3 +1,6 @@
+import 'package:first_app/pages/Yugioh.dart';
+import 'package:first_app/pages/formulario.dart';
+import 'package:first_app/pages/surprise.dart';
 import 'package:flutter/material.dart';
 
 class Index extends StatefulWidget {
@@ -8,64 +11,66 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  int _currentIndex = 0;
+
+  List _pages = [
+    Formularinho(),
+    Surprise(),
+    Yugioh(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Menu"),
-        ),
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Image.asset(
-                      "images/deadpoll.jpg",
-                      width: 200,
-                      height: 200,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child:
-                            ElevatedButton(
-                              onPressed: (){
-                                Navigator.pushNamed(context, "/form");
-                              },
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-                                child: Text(
-                                  "Converter temperatura",
-                                  style: TextStyle(fontSize: 14, color: Colors.white),
-                                ),
-                              ),
-                            )
-                        ),
-                        Expanded(
-                            child:
-                            ElevatedButton(
-                              onPressed: (){
-                                Navigator.pushNamed(context, "/surprise");
-                              },
-                              style: ElevatedButton.styleFrom(primary: Colors.orange),
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 28),
-                                child: Text(
-                                  "Surprise",
-                                  style: TextStyle(fontSize: 14, color: Colors.white),
-                                ),
-                              ),
-                            )
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-        )
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey),
+        backgroundColor: Colors.white,
+        title: Image.asset("images/yugioh2.png", scale: 8),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: (){},
+          ),
+        ],
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (_index){
+            setState(() {
+              _currentIndex = _index;
+            });
+          },
+          // ou shifting
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.red,
+                label: "Graus",
+                icon: Icon(Icons.ac_unit_outlined)
+            ),
+            BottomNavigationBarItem(
+                label: "Gift",
+                icon: Icon(Icons.whatshot)
+            ),
+            BottomNavigationBarItem(
+                label: "Inscrições",
+                icon: Icon(Icons.subscriptions)
+            ),
+            BottomNavigationBarItem(
+                label: "Biblioteca",
+                icon: Icon(Icons.folder)
+            ),
+          ]
+      ),
     );
   }
 }
